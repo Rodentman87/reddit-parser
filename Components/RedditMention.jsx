71be006151cfeda8bfe0fe7c1e43530bd3b1fc9f
Noah <rodentman87@gmail.com>
2020-10-31 13:17:14 -0500

    Make the parsing in the Mention component better

--- Components/RedditMention.jsx
+++ Components/RedditMention.jsx
@@ -1,33 +1,25 @@
-const { React } = require('powercord/webpack');
+const { React } = require("powercord/webpack");
 
 class RedditLink extends React.PureComponent {
-    render() {
-        const { redditLink } = this.props;
+  render() {
+    const { redditLink } = this.props;
 
-        if(redditLink.startsWith("/")){
-            return <a 
-                class={"reddit-mention"}
-                title={`https://reddit.com${redditLink}`}
-                rel={"noreferrer noopener"}
-                href={`https://reddit.com${redditLink}`}
-                role={"button"}
-                target={"_blank"}
-            >
-                {redditLink}
-            </a>;
-        } else {
-            return <a 
-                class={"reddit-mention"}
-                title={`https://reddit.com/${redditLink}`}
-                rel={"noreferrer noopener"}
-                href={`https://reddit.com/${redditLink}`}
-                role={"button"}
-                target={"_blank"}
-            >
-                {redditLink}
-            </a>;
-        }
-    }
+    // Filter out the leading / if it has one
+    const tag = redditLink.match(/\/?([ur]\/[a-zA-Z_-]{3,20})/)[1];
+
+    return (
+      <a
+        class={"reddit-mention"}
+        title={`https://reddit.com/${tag}`}
+        rel={"noreferrer noopener"}
+        href={`https://reddit.com/${tag}`}
+        role={"button"}
+        target={"_blank"}
+      >
+        {redditLink}
+      </a>
+    );
+  }
 }
 
 module.exports = RedditLink;
