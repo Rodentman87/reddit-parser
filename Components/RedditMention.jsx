f5cc6e0807ebe2463b64b97fceb67f79dd24e185
Noah <rodentman87@gmail.com>
2020-10-31 15:40:33 -0500

    Add tooltips to reddit tags with icon and NSFW tag

--- Components/RedditMention.jsx
+++ Components/RedditMention.jsx
@@ -1,6 +1,16 @@
-const { React } = require("powercord/webpack");
+const { React, getModuleByDisplayName } = require("powercord/webpack");
+const { get } = require("powercord/http");
 
-class RedditLink extends React.PureComponent {
+const Tooltip = getModuleByDisplayName("Tooltip", false);
+
+class RedditLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tooltip: "Loading...",
+    };
+  }
+
   render() {
     const { redditLink } = this.props;
 
@@ -8,17 +18,73 @@ class RedditLink extends React.PureComponent {
     const tag = redditLink.match(/\/?([ur]\/[a-zA-Z_\-0-9]{3,20})/)[1];
 
     return (
-      <a
-        class={"reddit-mention"}
-        title={`https://reddit.com/${tag}`}
-        rel={"noreferrer noopener"}
-        href={`https://reddit.com/${tag}`}
-        role={"button"}
-        target={"_blank"}
-      >
-        {redditLink}
-      </a>
+      <Tooltip position={"top"} text={this.state.tooltip}>
+        {(props) => (
+          <span {...props}>
+            <a
+              class={"reddit-mention"}
+              title={`https://reddit.com/${tag}`}
+              rel={"noreferrer noopener"}
+              href={`https://reddit.com/${tag}`}
+              role={"button"}
+              target={"_blank"}
+            >
+              {redditLink}
+            </a>
+          </span>
+        )}
+      </Tooltip>
     );
+  }
+
+  componentWillMount() {
+    this.getTooltip(
+      this.props.redditLink.match(/\/?([ur]\/[a-zA-Z_\-0-9]{3,20})/)[1]
+    ).then((tooltip) => {
+      this.setState({
+        tooltip,
+      });
+    }, this);
+  }
+
+  async getTooltip(tag) {
+    if (tag.startsWith("u")) {
+      // Tooltip for a user
+      const result = await get(
+        `https://www.reddit.com/user/${tag.substring(2)}/about.json`
+      );
+      const iconImage = result.body.data.icon_img.substring(
+        0,
+        result.body.data.icon_img.indexOf("?")
+      );
+      return (
+        <span>
+          {iconImage !== "" ? <img src={iconImage} style={{ width: "30px", height: "30px" }} /> : null}
+          <span class="reddit-tooltip">
+            {tag}
+            {result.body.data["over_18"] ? (
+              <span class="reddit-nsfw-tag">NSFW</span>
+            ) : null}
+          </span>
+        </span>
+      );
+    } else {
+      // Tooltip for a subreddit
+      const result = await get(`https://www.reddit.com/${tag}/about.json`);
+      const iconImage = result.body.data.icon_img;
+      return (
+        <span class="reddit-tooltip-wrapper">
+          {iconImage !== "" ? <img src={iconImage} style={{ width: "30px", height: "30px" }} /> : null}
+          <span class="reddit-tooltip">
+            {tag}
+            {result.body.data["over18"] ? (
+              <span class="reddit-nsfw-tag">NSFW</span>
+            ) : null}
+          </span>
+        </span>
+      );
+      return tag;
+    }
   }
 }
 
